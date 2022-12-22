04ac2d1dcf6e3fe8016799d11d9f72245072d192
Eder Vieira <eder.edy@gmail.com>
2022-12-21 23:00:09 -0400

    Concluido o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarthphone Nokia");
+Nokia nk = new Nokia("999887766", "Zenfone 5", "152489965874", 64);
+nk.Ligar();
+nk.ReceberLigacao();
+nk.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smarthphone Iphone");
+Iphone ip = new Iphone("933221166", "Iphone X", "98798745874", 256);
+ip.Ligar();
+ip.ReceberLigacao();
+ip.InstalarAplicativo("Telegram");
